3822528d527b35cc1fa5d48e2f9495dc28ed3d64
David D. Roberts <robertsdavidddr@gmail.com>
2012-03-15 21:12:25 -0700

    Updated stats visualizer to do chart deaths as blue. Improved colours. Unimproved alphas.

--- frogatto's stats.js
+++ frogatto's stats.js
@@ -14,6 +14,10 @@ function alert(msg, colour) {
 	document.getElementById("message").innerHTML = msg + '<br>'
 }
 
+function setup() {
+	0
+}
+
 function graph() {
 	alert('Loading…')
 	//<div style="width:32px; height:32px; background-color:#F00; position:absolute; left:150px; top:150px; opacity:0.4"></div>
@@ -41,25 +45,31 @@ function graph() {
 					type: 'get',
 					success: function(msg) {
 						alert('Processing…')
-						maxTiles = [Math.round((offset[2]-offset[0])/32), Math.round((offset[3]-offset[1])/32)] //.reduce(function(a,b){return Math.max(a.value,b.value)})
+						maxValue = [Math.round((offset[2]-offset[0])-33), Math.round((offset[3]-offset[1])-33)] //.reduce(function(a,b){return Math.max(a.value,b.value)})
 						zeros = [-offset[0], -offset[1]]
 						
-						theBitWithTables = msg[msg.map(function(oin){return oin.type}).indexOf("move")]
-						fullPowerMultiplier = Math.round(255 / (theBitWithTables.total / theBitWithTables.tables[0].entries.length / colour_intensity))
-						
-						grid = []; grid.length = maxTiles[0]	//construct a grid of a certain size
-						$.each(grid, function(index){grid[index] = []; grid[index].length = maxTiles[1]})
-						
-						safeSetConstrainedToGrid = function(coord, value){
-							grapher.innerHTML += "<div style=\"width:32px; height:32px; background-color:#F00; position:absolute; left:" + (coord[0]+zeros[0]) + "px; top:" + (coord[1]+zeros[1]) + "px; opacity:" + value/250 + "\"></div>"
+						graphStuff = function(theBitWithTables, colour, intensityDivisor) {
+							safeSetConstrainedToGrid = function(coord, value){
+								coord[0] = Math.min(Math.max(coord[0], offset[0]), maxValue[0])
+								coord[1] = Math.min(Math.max(coord[1], offset[1]), maxValue[1])
+								grapher.innerHTML += "<div style=\"width:32px; height:32px; background-color:" + colour + "; position:absolute; left:" + (coord[0]+zeros[0]) + "px; top:" + (coord[1]+zeros[1]) + "px; opacity:" + value/intensityDivisor + "\"></div>"
+							}
+							
+							//safeSetConstrainedToGrid([50,50], 2)
+							$.each(theBitWithTables.tables[0].entries,function(index, value){
+								key = value.key
+								key[0] -= 16; key[1] -= 16
+								safeSetConstrainedToGrid(key, value.value)
+							})
 						}
 						
-						//safeSetConstrainedToGrid([50,50], 2)
-						$.each(theBitWithTables.tables[0].entries,function(index, value){
-							key = value.key
-							key[0] -= 16; key[1] -= 16
-							safeSetConstrainedToGrid(key, value.value*fullPowerMultiplier)
-						})
+						theBitWithTables = msg[msg.map(function(oin){return oin.type}).indexOf("move")]
+						graphStuff(theBitWithTables, '#F80', 6) //calculate this '6' based on how much data we have in the level
+						
+						theBitWithTables = msg[msg.map(function(oin){return oin.type}).indexOf("die")]
+						graphStuff(theBitWithTables, '#08F', 4)
+						
+						
 						alert('')
 					},
 				})

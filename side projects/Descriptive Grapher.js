5744f1688f1e2373018180adb550bb338fb31b94
David D. Roberts <robertsdavidddr@gmail.com>
2014-01-21 01:15:00 -0800

    Made the descriptive grapher graphs move, but it only takes into account x motion. In unsolvable cases, it panics.

--- side projects/Descriptive Grapher.js
+++ side projects/Descriptive Grapher.js
@@ -1,27 +1,29 @@
 /* global _, console */
-/* jshint globalstrict: true */
 
-"use strict";
-
-var graph, graphs = [], gWidth, gHeight, g, text;
+var graph, graphs = [], g, text;
 var init = function() {
+	"use strict";
 	document.getElementById('intro').style.display="none";
 	document.getElementById('main').style.display="block";
 	graph = document.getElementById('graphs');
-	graph.addEventListener('mousemove', graphMouseMove);
-	graph.addEventListener('click', graphClick);
+	addEventListeners(graph);
 	g = graph.getContext('2d');
 	g.setLineDash = g.setLineDash || function() {};
 	text = document.getElementById('outputs');
 }; 
 
+var gWidth, gHeight, midx, midy, scale;
 var reflow = function() {
+	"use strict";
 	var footer = document.getElementById('footer');
 	gWidth = graph.width = Math.min(document.getElementById('main').clientWidth-text.clientWidth, 1000);
-	gHeight = graph.height = Math.min(Math.max(graph.width, text.clientHeight), document.body.clientHeight-footer.clientHeight - 50, 800);
+	gHeight = graph.height = Math.min(Math.max(graph.width, text.clientHeight), document.body.clientHeight-footer.clientHeight - 75, 800);
+	midx = gWidth/2, midy = gHeight/2;
+	scale = gWidth/Math.PI/zoom;
 };
 
 document.body.onload = function() {
+	"use strict";
 	graphs.push(getGraphable('line', 0.0,0.5,0.0));
 	graphs.push(getGraphable('wave', 0.5,0.5,0.0));
 	graphs.push(getGraphable('bowl', 0.0,0.0,0.5));
@@ -43,36 +45,51 @@ bell: y = a*pow(E, -(1/(2*pow(b, 2))*pow(x-c, 2)))+d //Rearranged for standard a
 */
 
 var getGraphable = function(type, r,g,b) {
+	"use strict";
 	return {
 		scalex: 1, //These values are subbed into the formula, and are used to determine how the formula follows the mouse.
 		scaley: 1,
 		offsetx: 0,
 		offsety: 0,
 		bits: {
+			//"line": ["", "*(", "*x-", ")+", ""],
+			//"wave": ["", "*Math.sin(", "*x-", ")+", ""],
+			//"bowl": ["", "*Math.pow(", "*x-", ", 2)+", ""],
+			//"bell": ["", "*Math.pow(Math.E, -(1/(2*Math.pow(", ", 2))*Math.pow(x-", ", 2)))+", ""],
 			"line": ["", "*(", "*x-", ")+", ""],
-			"wave": ["", "*Math.sin(", "*x-", ")+", ""],
-			"bowl": ["", "*Math.pow(", "*x-", ", 2)+", ""],
-			"bell": ["", "*Math.pow(Math.E, -(1/(2*Math.pow(", ", 2))*Math.pow(x-", ", 2)))+", ""],
+			"wave": ["", "*sin(", "*x-", ")+", ""], //TODO: This should probably use a plain string with abcd vars now. See genFunction TODO as well.
+			"bowl": ["", "*pow(", "*x-", ", 2)+", ""],
+			"bell": ["", "*pow(E, -(1/(2*pow(", ", 2))*pow(x-", ", 2)))+", ""],
 		}[type],
 		colour: [r,g,b,1],
 		dirty: true, //If we need to re-eval the the formula.
 		descriptionPosition: 0, //a midy
+		moveHint: {
+			scalex: 0.1/zoom, 
+			scaley: 0.1/zoom, 
+			offsetx:0.1/zoom, 
+			offsety:0.1/zoom
+		}, //A good initial guess on how far to move the factors of the function to make them meet our target, based on what the last value was. Probably a good idea to clamp this to 0.1 for now, with some multiplier for scale. Needs to be smaller than 1/4th of a periodic function, such as sin(x).
 	};
 };
 
 var genFunction = function(graphable) {
+	//This is the ugly bit of the codebase. Ideas welcome. :)
+	//Apparently removing the global strict nerfs performance in FF. The alternative is to import the math function manually, which is… ugly.
+	//Update: Fixed some stuff with regards to mouse move always rerendering the field, and FF is responsive again. I'm guessing FF queues the mouse move events to some degree?
 	var f, g=graphable; 
 	var a=g.scalex, b=g.scaley, c=g.offsetx, d=g.offsety; //TODO: Sub this into the code below so we can just go abcd for stuff.
-	eval("f = function line(x) {return " + g.bits[0] + " " + g.scalex + " " + g.bits[1] + " " + g.scaley + " " + g.bits[2] + " " + g.offsetx + " " + g.bits[3] + " " + g.offsety + " " + g.bits[4] + ";}"); //Spaces to avoid x-n where n is -1 becoming x--1, which is an illegal decrement operator.
+	with(Math) {
+		eval("f = function line(x) {return " + g.bits[0] + " " + g.scalex + " " + g.bits[1] + " " + g.scaley + " " + g.bits[2] + " " + g.offsetx + " " + g.bits[3] + " " + g.offsety + " " + g.bits[4] + ";}"); //Spaces to avoid x-n where n is -1 becoming x--1, which is an illegal decrement operator.
+	}
 	return f;
 };
 
-var renderCache = [], resolution = 1, zoom = 4, padding = 1.1; var halfpad = (padding-1)/2+1;
+var renderCache = [], resolution = 1, zoom = 4, padding = 1.1, halfpad = (padding-1)/2+1; //Note, might want to scale resolution by zoom and "c", or scaley.
 renderCache.dirty = true;
 var render = function(graphs) {
+	"use strict";
 	g.clearRect(0,0,gWidth,gHeight);
-	var midx = gWidth/2, midy = gHeight/2;
-	var scale = gWidth/Math.PI/zoom;
 	var x=0, y=0;
 	
 	//Draw the guides.
@@ -121,13 +138,14 @@ var render = function(graphs) {
 			var lastGoodY = midy;
 			x = -midx;
 			g.beginPath();
-			g.moveTo(midx+x/padding, (midy*padding-(f(x/(scale))||0)*scale)/padding);
+			//g.moveTo(midx+x/padding, (midy*padding-(f(x/(scale))||0)*scale)/padding); //Not needed, I guess? Mysteries abound.
 			for(x; x < midx+resolution; x+=resolution) {
-				y =                   midy*padding-(f(x/(scale))||0)*scale;
-				g.lineTo(midx+x/padding, y/padding);
+				y = scaledFnValue(f, x);
+				g.lineTo(scaledX(x), y);
 				if(y >= -midy/padding+midy && y <= +midy/padding+midy) {lastGoodY = y;}
 			}
 			g.setStrokeColor ? g.setStrokeColor.apply(g, graphable.colour) : g.strokeStyle = "rgba("+graphable.colour.map(function(x) {return Math.round(x*255);}).join()+")";
+			g.setLineWidth ? g.setLineWidth(index !== highlight ? 1 : 2) : g.lineWidth = (index !== highlight ? 1 : 2);
 			g.stroke();
 			return lastGoodY;
 		}
@@ -137,14 +155,22 @@ var render = function(graphs) {
 	
 	graphys.forEach(function(goodY, index) {
 		g.beginPath();
-		g.moveTo(gWidth/(halfpad)+5, goodY/padding);
+		g.moveTo(gWidth/(halfpad)+5, Math.max(Math.min(scaledY(midy), scaledFnValue(renderCache[index], midx)), scaledY(-midy)));
 		g.lineTo(gWidth, index*15+7);
 		g.stroke();
 	});
 };
 
+var scaledFnValue = function(f, x) {
+	"use strict";
+	return (midy*padding-(f(x/scale)||0)*scale)/padding; 
+};
+var scaledX = function(x) {"use strict"; return midx+x/padding;};
+var scaledY = function(x) {"use strict"; return midy+x/padding;};
+
 var textCache = document.createDocumentFragment();
 var write = function(graphs) {
+	"use strict";
 	text.innerHTML = "";
 	textCache.innerHTML = "";
 	graphs.forEach(function(graph, index) {
@@ -180,10 +206,9 @@ var write = function(graphs) {
 		};
 		
 		var increment = function(place, event) {
-			console.log(event);
 			event.target.textContent = 
 				graph[['scalex','scaley','offsetx','offsety'][place]] += 
-				event.deltaX + event.deltaY + event.deltaZ < 0 ? +1 : -1;
+				event.deltaX + event.deltaY + event.deltaZ < 0 ? +1 : -1; //FF and Chrome inconsistent about axis.
 			graph.dirty = true;
 			render(graphs);
 		};
@@ -252,15 +277,118 @@ var write = function(graphs) {
 	text.appendChild(textCache);
 };
 
-var nearestFunction = function(x,y) {
-	console.log('got', x, y);
-	return {dist: 100, index: 1};
+var distanceToNearestFunction = function(spread, functions, x,y) { //x/y in terms of graph-space
+	"use strict";
+	return functions.
+		map(function(f) {
+			return _.range(-spread,spread+1). //TODO: Penalize ends.
+				map(function(ox) {return x+ox-midx;}).
+				map(function(x) {return scaledFnValue(f, x);}).
+				map(function difference(ny) {return y-ny;}).
+				reduce(function(a,b) {return Math.abs(a) < Math.abs(b) ? a : b;});
+		}).
+		map(function(d,i) {return {dist:d, index:i};}).
+		reduce(function(prev, next) {return Math.abs(prev.dist) < Math.abs(next.dist) ? prev : next;});
 };
 
-var graphMouseMove = function(event) {
-	console.log('m', event);
-};
-
-var graphClick = function(event) {
-	console.log('c', event);
+var highlight = -1;
+var addEventListeners = function(graph) {
+	"use strict";
+	
+	var offset = graph.getBoundingClientRect();
+	var distanceToNearestFunctionFromMouse = function(spread, functions, x,y) {
+		return distanceToNearestFunction(spread, functions, 
+			(x-offset.left-midx)*padding+midx, y-offset.top);
+	};
+	
+	var previousMouseX, previousMouseY, isMoveCursor = false, previouslyHighlighted = -1;
+	graph.addEventListener('mousemove', function(event) {
+		var mouseX = event.x||event.clientX;
+		var mouseY = event.y||event.clientY;
+		
+		if(mouseButtons[0]) {
+			if(highlight !== -1) {
+				var deltaX = mouseX-previousMouseX;
+				var deltaY = mouseY-previousMouseY;
+				//console.log('dragging', highlight, deltaX, deltaY);
+				var fac = !isMoveCursor ? {x:'offsetx', y:'offsety'} : {x:'scalex', y:'scaley'};
+				//Move function by guess.
+				//Test function. Take percentage we're off by and multiply guess by that.
+				//Test function. If off, factor is non-linear.
+				 //Progress by the 2x the part of the factor we were off by. eg, expected 10, got 9, factor is 0.1, try again with 0.12 as the factor.
+				  //If that fails, try reversing direction and seeing if we get closer. If we do, continue until we overshoot.
+				 //Binary search between the two extremes. Max recursion = 9, tolerance = whatever 0.5px comes out to in graph-land.
+				
+				//Figure out both at once, we need the ratio data…
+				var oldCurve = graphs[highlight];
+				var newCurve = _.clone(graphs[highlight]);
+				newCurve.dirty = true;
+				
+				var currentDist, guessedDist, scale;
+				if(deltaX) {
+					newCurve[fac.x] = oldCurve[fac.x] + (deltaX * oldCurve.moveHint[fac.x]);
+					currentDist = distanceToNearestFunctionFromMouse(0, [renderCache[highlight]], mouseX, previousMouseY).dist;
+					guessedDist = distanceToNearestFunctionFromMouse(0, [genFunction(newCurve) ], mouseX, previousMouseY).dist;
+					scale = Math.abs(currentDist/(currentDist-guessedDist));
+					newCurve.moveHint[fac.x] = oldCurve.moveHint[fac.x]*scale;
+					newCurve[fac.x] = oldCurve[fac.x] + (deltaX * newCurve.moveHint[fac.x]);
+					graphs[highlight] = newCurve;
+					render(graphs);
+				}
+				
+				var ratio = deltaX/deltaY;
+				
+			}
+		} else { //Perhaps this bit should be callable from the mouseup function, you can drag nothing onto a line and it doesn't update the graphics.
+			var dtf = distanceToNearestFunctionFromMouse(5, renderCache, 
+				previousMouseX, previousMouseY);
+			highlight = Math.abs(dtf.dist) > 10 ? -1 : dtf.index;
+			if(previouslyHighlighted != highlight) {
+				previouslyHighlighted = highlight;
+				render(graphs);
+			}
+			
+			updateMouse();
+		}
+		
+		previousMouseX = mouseX; 
+		previousMouseY = mouseY;
+	});
+	
+	var updateMouse = function() {
+		if(isMoveCursor !== highlight < 0) {
+			isMoveCursor = highlight < 0; 
+			graph.className = highlight < 0 ? "" : mouseMoves ? "cursor-move" : "cursor-resize";
+		}
+	};
+	
+	var mouseButtons = {};
+	document.body.addEventListener('mousedown', function(event) {
+		mouseButtons[event.button] = true;
+	});
+	document.body.addEventListener('mouseup', function(event) {
+		mouseButtons[event.button] = false;
+	});
+	document.body.addEventListener('mouseout', function() {
+		mouseButtons = {};
+	});
+	
+	
+	graph.addEventListener('mousedown', function(event) {
+		event.preventDefault();
+	});
+	graph.addEventListener('contextmenu', function(event) {
+		event.preventDefault();
+	});
+	
+	var move = document.getElementById('move'), stretch = document.getElementById('scale');
+	var mouseMoves = move.checked;
+	move.addEventListener('change', function() {
+		mouseMoves = true, isMoveCursor = null; //null just invalidates a cache for updateMouse written by the movement function.
+		updateMouse();
+	});
+	stretch.addEventListener('change', function() {
+		mouseMoves = false, isMoveCursor = null;
+		updateMouse();
+	});
 };

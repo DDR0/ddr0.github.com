d029837f183cba6110bcd98cab321aca46ae2a8f
DDR <d@ddr0.ca>
2020-04-05 23:07:02 -0700

    Removed deletion of old files from deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,25 @@
-rsync --recursive --delete --links --times --itemize-changes \
+rsync --recursive --links --times --itemize-changes \
 	./ "cac2:/var/www/html/" \
 	--exclude=".*" \
 	--exclude="site-config" \
 	--exclude="*.node.js" \
 	--exclude="node_modules" \
 	&
-rsync --itemize-changes site-config/ddr0 cac2:/etc/nginx/default/ddr0 &
-rsync --itemize-changes site-config/dice.service cac2:/lib/systemd/system/ &
-rsync --itemize-changes 🎲/dice.node.js cac2:/opt/ddr0.ca/🎲/ &
-rsync --recursive --delete --links --times --itemize-changes \
-	node_modules cac2:/opt/ddr0.ca/ &
+
+rsync --recursive --links --times --itemize-changes \
+	site-config/ddr0 cac2:/etc/nginx/default/ddr0 \
+	&
+
+rsync --recursive --links --times --itemize-changes \
+	site-config/dice.service cac2:/lib/systemd/system/ \
+	&
+
+rsync --recursive --links --times --itemize-changes \
+	🎲/dice.node.js cac2:/opt/ddr0.ca/🎲/ \
+	&
+
+rsync --recursive --links --times --itemize-changes \
+	node_modules cac2:/opt/ddr0.ca/ \
+	&
+
 wait

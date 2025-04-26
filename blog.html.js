5b94a0c551d4715bf467f576b9b8f83b3d9cdeb5
DDR <d@ddr0.ca>
2025-04-26 11:25:40 -0700

    Blog: Verify Mastodon URL with a backlink.

--- blog.html.js
+++ blog.html.js
@@ -66,6 +66,7 @@ ${include('site shell intro.html.frag.js', {
 		`<link rel="alternate" type="application/rss+xml" title="DDR's Blog" href="/blog-rss-feed.xml" />`,
 		`<link href='/css/prism-okaidia.css' rel='stylesheet'>`,
 		`<script defer src='/scripts/unindent-code-blocks.js'></script>`, //Can't be async, mucks up the first code block in /blog-posts/15.Hydrating_Objects. (Seems to be fighting Prism.)
+		`<link rel="me" href="https://pony.social/deck/@ddr">`, //Verify Mastodon profile URL.
 	],
 })}
 
